9ae29980f86be7081c93dc7ddfbe5eb2e6ad2b90
Francesc Forés <ffores93@gmail.com>
2024-03-05 22:21:08 +0100

    vercel

--- src/app/services/interceptors/jwt.interceptor.ts
+++ src/app/services/interceptors/jwt.interceptor.ts
@@ -16,8 +16,7 @@ export class JwtInterceptor implements HttpInterceptor {
 
       request = request.clone({
         setHeaders: {
-          // Authorization: `Bearer ${currentUser.token}`
-          Authorization: `Bearer qLbj4T2O1Iapu3SPgjFFkahd`
+          Authorization: `Bearer ${currentUser.token}`
         }
       });
     }
